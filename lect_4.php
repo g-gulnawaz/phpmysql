724a32bcedfc0f1a110915850df7cee2f70988ac
eme <gulnawazgani@gmail.com>
2026-09-01 10:29:12 +0530

    first commit#

---
+++ lect_4.php
@@ -0,0 +1,21 @@
+<?php
+// PHP Variables and data types 
+/* operators
+arithmetic: +, -, *, /, **
+assignment: =, +=, -=, *=, /=
+comparison: ==, ===, !=, !==, <, >, <=, >=, <=>(spaceship)
+logical: &&, ||, !,AND, or
+string operators: ., .= (concatenation and assign)
+*/
+
+$num1 = 10;
+$num2 = 20;
+echo "Sum = ".($num1+$num2)."<br>";
+echo "Difference = ".($num1-$num2)."<br>";
+echo "Prodcut = ".($num1*$num2)."<br>";
+echo "Division = ".($num2/$num1)."<br>";
+echo "Division = ".($num1/$num2)."<br>";
+echo "Expeonentiation = ".($num2**$num2)."<br>";
+
+
+?>
